ffa6d14bdda0535ba75370af17b9531eae53f8e3
otcova <otgercomas@gmail.com>
2022-10-25 15:39:57 +0200

    Prevent cheats

--- src/Scenes/OpenServer.js
+++ src/Scenes/OpenServer.js
@@ -1,12 +1,14 @@
+import childProcess from "child_process"
 import React from "react"
-import { SceneLockServer } from "./LockServer"
+import { changeScene } from ".."
 import * as DataBase from "../DataBase"
 import { SceneButtons } from "./Buttons"
-import childProcess from "child_process"
+import { SceneLockServer } from "./LockServer"
 import { SceneUnlockServer } from "./UnlockServer"
-import { changeScene } from ".."
 
 import path from "path"
+
+let mc_process = null;
 
 export class SceneOpenServer extends React.Component {
 	constructor(props) {
@@ -17,12 +19,25 @@ export class SceneOpenServer extends React.Component {
 			electron.ipcRenderer.send('close-window')
 	}
 	render() {
-		if (this.state.text == "Closing Server")
-			setTimeout(()=>electron.ipcRenderer.send('hide-window'),0)
+		// if (this.state.text == "Server Is Running")
+		// 	setTimeout(()=>electron.ipcRenderer.send('hide-window'),0)
 
-		return <div className="exp row-center">
-			<p>{this.state.text}</p>
-		</div>
+		if (mc_process) {
+			return <div className="exp row-center">
+				<p>{this.state.text}</p>
+				<button style={{marginLeft: "20px"}} onClick={() => {
+					if (mc_process) mc_process.stdin.write("stop\n");
+					else {
+						console.error("Server process has terminated without notifying");
+						changeScene(SceneUnlockServer)
+					}
+				}}>Stop Server</button>
+			</div>
+		} else {
+			return <div className="exp row-center">
+				<p>{this.state.text}</p>
+			</div>
+		}
 	}
 	componentDidMount() {
 		if (!this.props.sceneData.close)
@@ -32,33 +47,34 @@ export class SceneOpenServer extends React.Component {
 	async openServer() {
 
 		// Lock Server
-		
+
 		const currentHost = await DataBase.getCurrentHost()
 		if (currentHost != DataBase.getUserId()) {
 			if (currentHost == null) return changeScene(SceneLockServer)
 			else return changeScene(SceneButtons)
 		}
-		
+
 		// Open Server
 
 		this.setState({ text: "Opening Server" })
-		
+
 		if (await DataBase.getCurrentHost() == DataBase.getUserId()) {
 			const dir = path.resolve(git.baseDir, "Survival - Paper")
-			const cp = childProcess.spawn('cmd', ['/c', `java -Xmx${DataBase.getMemory()}G -Xms${DataBase.getMemory()}G -jar server.jar gui`], { cwd: dir })
-			// cp.stdout.on("data", this.cpReceive.bind(this))
+			mc_process = childProcess.spawn('cmd', ['/c', `java -Xmx${DataBase.getMemory()}G -Xms${DataBase.getMemory()}G -jar server.jar nogui`], { cwd: dir })
+			mc_process.stdout.on("data", b => console.log(b.toString()))
 
 			setTimeout(() => {
-				this.setState({ text: "Closing Server" })
+				this.setState({ text: "Server Is Running" })
 			}, 6000)
 
 			await new Promise((resolve) => {
-				cp.on("close", () => {
+				mc_process.on("close", () => {
 					electron.ipcRenderer.send('show-window')
 					resolve()
 				})
 			})
 
+			mc_process = null;
 			return changeScene(SceneUnlockServer, { close: true })
 		} else {
 			return changeScene(SceneButtons)
